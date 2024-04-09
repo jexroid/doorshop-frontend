4d66b9682737e62169b3510001fab3acb3e9df64
jexroid <amirfr2290@gmail.com>
2024-04-09 22:46:10 +0330

    git problem fixed

--- src/music.js
+++ src/music.js
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
-import {Howl, Howler} from 'howler';
-
-
-const buttons = document.querySelectorAll('.btn');
-=======
 import { Howl, Howler } from 'howler';
 
 
 const buttons = document.querySelectorAll('.btn');
 let clicked = false
->>>>>>> 247d72b (init)
 
 let sound = new Howl({
     src: ['/click.ogg'],
     html5: true
 });
 
-<<<<<<< HEAD
-buttons.forEach(el => el.addEventListener('click', () => {
-    sound.play()
-}));
-
-
-=======
 let musicbackground = new Howl({
     src: ['/generic.ogg'],
     loop: true
@@ -37,4 +23,3 @@ buttons.forEach(el => el.addEventListener('click', () => {
 window.addEventListener('load', function () {
     musicbackground.play()
 })
->>>>>>> 247d72b (init)
